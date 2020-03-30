fd5ca0f5d0cd5d5921bec2c49f56ad223e1803a5
Sudeep Basnet <sudbasnet10@gmail.com>
2020-03-29 20:35:52 -0500

    resume added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
+import { PageSelectService } from './page-select.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  providers: [PageSelectService],
 })
 export class AppComponent {
   title = 'sudbasnet';
+  currentPage = 'home';
 
+  constructor(private pageSelectService: PageSelectService) {
+    pageSelectService.pageUpdated.subscribe((page: string) => {
+      this.currentPage = page;
+    })
+  }
   ngOnInit() {
   }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { PageSelectService } from '../page-select.service';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  @Input() currentPage;
+  constructor(private pageSelectService: PageSelectService) { }
 
   ngOnInit(): void {
+  }
+
+  onSelectPage(page: string) {
+    this.pageSelectService.pageUpdated.emit(page);
   }
 
 }
