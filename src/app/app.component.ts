6144cd4f7519be23f0891fd224f3a0db8e3f84e7
Sudeep Basnet <sudbasnet10@gmail.com>
2020-04-28 16:40:30 -0500

    fixed footer issue in /resume page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { PageSelectService } from './page-select.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -9,13 +12,17 @@ import { PageSelectService } from './page-select.service';
 })
 export class AppComponent {
   title = 'sudbasnet';
-  currentPage = 'home';
+  currentPage: string;
 
-  constructor(private pageSelectService: PageSelectService) {
-    pageSelectService.pageUpdated.subscribe((page: string) => {
-      this.currentPage = page;
+  constructor(private router: Router) {
+    // pageSelectService.pageUpdated.subscribe((page: string) => {
+    //   this.currentPage = page;
+    router.events.subscribe(val => {
+      this.currentPage = router.url;
     })
+
   }
+
   ngOnInit() {
   }
 }
