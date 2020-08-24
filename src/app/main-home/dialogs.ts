0d32763a0ab532c352353df8c969196f32d6ccca
Sudeep Basnet <sudbasnet10@gmail.com>
2020-08-24 11:05:44 -0500

    updated dialogs and resume

--- src/app/main-home/dialogs.ts
+++ src/app/main-home/dialogs.ts
@@ -12,7 +12,7 @@ const jsonText = [
     {
         "title": "conversation",
         "tags": "",
-        "body": "hmm..\n[[Tell me about yourself.|intro]]\n[[Your website is not too bad!|idle]]\n",
+        "body": "hmm..\n[[Tell me about yourself.|intro]]",
         "position": {
             "x": -595,
             "y": -134
@@ -32,9 +32,9 @@ const jsonText = [
     {
         "title": "intro",
         "tags": "",
-        "body": "Well, I have 4.5 years of professional experience in Software Dev, and I recently earned a Master's degree in Computer Science.\n[[Tell me about your professional experience.|work]]\n[[When did you graduate?|mscs]]\n[[Where did you graduate from?|mscs]]\n\n",
+        "body": "I am currently working as a Technical Consultant in Minnepolis. In 2019, I got my Master's in Computer Science. Before that, I had 4.5 years of professional experience as a Software Eng in Data Operations. \n[[Tell me about your current position.|logic]]\n[[Where did you graduate from?|mscs]]\n[[Tell me about your Software Eng. work.|work]]\n\n",
         "position": {
-            "x": -286,
+            "x": -288,
             "y": -130
         },
         "colorID": "0"
@@ -42,7 +42,7 @@ const jsonText = [
     {
         "title": "mscs",
         "tags": "",
-        "body": "I got my M.Sc. degree in Aug-2019 from the University of Nebraska - Lincoln.\n[[Why did you decide to get a Master's degree?|whymasters]]\n[[What was your focus on during your studies?|gradwork]]\n[[And what was your professional work about?|work]]",
+        "body": "I got my M.Sc. degree in Aug-2019 from the University of Nebraska - Lincoln.\n[[Why did you get a Master's degree?|whymasters]]\n[[What was your focus on during your studies?|gradwork]]\n[[And what was your previous work about?|work]]",
         "position": {
             "x": 7,
             "y": 138
@@ -52,7 +52,7 @@ const jsonText = [
     {
         "title": "jobsearch",
         "tags": "",
-        "body": "I'm looking for jobs right now.\n[[What are your interests?|interests]]",
+        "body": "I'm open to new opportunities\n[[What are your interests?|interests]]",
         "position": {
             "x": 657,
             "y": -137
@@ -62,7 +62,7 @@ const jsonText = [
     {
         "title": "interests",
         "tags": "",
-        "body": "My interests are in Software Development and Data Science.\n[[What kind of skills you got?|skills]]\n[[How do I contact you?|email]]",
+        "body": "I think of myself as a GENERALIST, I have varying interests (from web dev to game dev to digital art to scientific research) but they all fall under the umbrellas of Software Development and Data Science. \n[[What kind of skills you got?|skills]]\n[[How do I contact you?|email]]",
         "position": {
             "x": 939,
             "y": -136
@@ -72,7 +72,7 @@ const jsonText = [
     {
         "title": "skills",
         "tags": "",
-        "body": "I have professional experience with Java and Oracle on Windows and Linux systems. I'm pretty good with Python, Django, R, JavaScript, React, PHP and HTML/CSS as well.\n[[Lets look at your resume.|resume]]\n[[How can we get in touch?|email]]",
+        "body": "I'm pretty good with Python, C#, Javascript (Node), R, Angular, PHP and HTML/CSS. Experienced in Data Analysis, Data Visualization, Database Development, Systems Admin.(LINUX), Cloud DataWarehousing, Oracle.\n[[Lets look at your resume.|resume]]\n[[How can we get in touch?|email]]",
         "position": {
             "x": 1227,
             "y": -137
@@ -82,17 +82,17 @@ const jsonText = [
     {
         "title": "work",
         "tags": "",
-        "body": "I primarily worked with Java and Oracle PL/SQL to develop ETL tools and databases for large-scale Enterprise data solutions.\n[[So, why do Masters?|whymasters]]\n[[Did you also work during your studies?|gradwork]]\n[[Where can I find more details?|resumepre]]\n",
+        "body": "I worked with Oracle PL/SQL and Java to develop ETL tools and databases for large-scale Enterprise data solutions.\n[[So, why do Masters?|whymasters]]\n[[Did you also work during your studies?|gradwork]]\n[[Where can I find more details?|resumepre]]\n",
         "position": {
-            "x": 8,
-            "y": -135
+            "x": 9,
+            "y": -206
         },
         "colorID": "0"
     },
     {
         "title": "email",
         "tags": "",
-        "body": "You can contact me through email: sudbasnet10@gmail.com\n[[Oh, and your resume? |resume]]",
+        "body": "You can contact me through email: sudbasnet10@gmail.com\n[[And your resume? |resume]]",
         "position": {
             "x": 1061,
             "y": 142
@@ -102,17 +102,17 @@ const jsonText = [
     {
         "title": "gradwork",
         "tags": "",
-        "body": "I was primarily involved in research and development on spatial-data mining and Agent-based Modeling. I used Python, Java, R, JavaScript and SQL.\n[[I'd like to know more.|resumepre]]\n[[What's next?|jobsearch]]\n",
+        "body": "I was primarily involved in research and development on spatial-data mining and Agent-based Modeling. I used Python, Java, R, JavaScript and SQL.\n[[I'd like to know more.|resumepre]]\n[[Still looking for jobs?|jobsearch]]\n",
         "position": {
-            "x": 359,
-            "y": -142
+            "x": 370,
+            "y": -126
         },
         "colorID": 0
     },
     {
         "title": "whymasters",
         "tags": "",
-        "body": "I am a career-oriented person, I decided to study to further my knowledge in Computer Science, and for better work opportunities. \n[[How did it go?|gradwork]]",
+        "body": "I thought I had some gaps in my knowledge, so I decided to study Computer Science ^600 ... ^600 It was also for getting better job oppurtunities :)\n[[How did it go?|gradwork]]",
         "position": {
             "x": 355,
             "y": 139
@@ -122,30 +122,30 @@ const jsonText = [
     {
         "title": "resumepre",
         "tags": "",
-        "body": "Please have a look at my resume\n[[What are you up to these days?|jobsearch]]",
+        "body": "Please have a look at my resume.\n[[Are you open to new job opportunities?|jobsearch]]",
         "position": {
-            "x": 363,
-            "y": -427
-        },
-        "colorID": 0
-    },
-    {
-        "title": "idle",
-        "tags": "",
-        "body": "Cool\n[[Ok, now tell me about yourself. |intro]]",
-        "position": {
-            "x": -600,
-            "y": 163
+            "x": 373,
+            "y": -392
         },
         "colorID": 0
     },
     {
         "title": "resume",
         "tags": "",
-        "body": "Please find my resume at: \n[[RESTART! |Start]]",
+        "body": "Please find my resume in the header above.\n[[RESTART!|Start]]",
         "position": {
             "x": 1413,
             "y": 139
+        },
+        "colorID": 0
+    },
+    {
+        "title": "logic",
+        "tags": "",
+        "body": "I've been working at Logic since Oct-2019 as a Technical Consultant, functioning as a .NET Developer. My Expertise is in Software and Database Dev, Data Warehousing, and Reporting.\n[[And the work before that?|work]]\n[[And your masters?|mscs]]",
+        "position": {
+            "x": -296,
+            "y": 132
         },
         "colorID": 0
     }
